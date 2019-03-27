0f5993c45f6556c5fb773bff9489b31c323593de
antonio <malg61@gmail.com>
2019-03-27 15:13:15 +0100

    Modal de la vista de error. No tienes permiso

--- application/views/error.php
+++ application/views/error.php
@@ -1,1 +1,26 @@
-<h1>NO TIENES PERMISO PARA ACCEDER</h1>
+<script>
+    $(document).ready(function(){
+        $("#check_user").modal('toggle');
+    });
+</script>
+
+<div class="modal fade" id="check_user" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Advertencia</h5>
+        
+        </button>
+      </div>
+      <div class="modal-body">
+      <span id='error' class="fas fa-exclamation-circle"></span>
+        <h6 class='modal_mensaje_error'> No tienes permiso para acceder </h6>
+      </div>
+      <div class="modal-footer">
+      <?php
+      echo anchor('Login/index/','Volver al menu principal','type="button" class="btn btn-primary"');
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
