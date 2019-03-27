5245894446df6b4fe7bf7c8b0b2d34f5c6cc77c3
antonio <malg61@gmail.com>
2019-03-27 17:58:22 +0100

    Proveedores CRUD

--- application/controllers/Proveedores.php
+++ application/controllers/Proveedores.php
@@ -36,9 +36,36 @@ class Proveedores extends Security {
             redirect('Proveedores/index');
 
         }
+    }
+
+    public function delete ($id) {
+        
+        $r = $this->modelProveedores->delete($id);
+
+        if ($r == 0) {
+            //error
+            $data["msg"] = "1";
+            $this->session->set_flashdata('data',$data);
+            redirect('Proveedores/index');
+        } else {
+            //bien
+            $data["msg"] = "0";
+            $this->session->set_flashdata('data',$data);
+            redirect('Proveedores/index');
+
+        }
+    }
+
+    public function update ($id) {
+        $nombre = $this->input->get_post('upd_nombre');
+        $apellido1 = $this->input->get_post('upd_apellido1');
+        $apellido2 = $this->input->get_post('upd_apellido2');
+        $dni = $this->input->get_post('upd_dni');
+        $telf = $this->input->get_post('upd_telefono');
+
+        $r = $this->modelProveedores->update($id,$nombre, $apellido1, $apellido2, $dni, $telf);
 
     }
-    
 
     
 }
--- application/models/modelProveedores.php
+++ application/models/modelProveedores.php
@@ -17,4 +17,26 @@ class modelProveedores extends CI_Model {
         $query = $this->db->query("INSERT INTO proveedores (id, nombre, apellido1, apellido2, dni, telf) VALUES (null,'$nombre', '$apellido1', '$apellido2', '$dni', $telf);"); 
         return $this->db->affected_rows();
     }
+
+    public function delete($id){
+        $query = $this->db->query("DELETE FROM proveedores WHERE id = '$id'"); 
+        return $this->db->affected_rows();
+    }
+
+    public function update($id,$nombre, $apellido1, $apellido2, $dni, $telf){
+        
+        $status = 0;
+        $this->db->trans_start();
+        $query = $this->db->query("DELETE FROM proveedores WHERE id = '$id'"); 
+        $query = $this->db->query("INSERT INTO proveedores (id, nombre, apellido1, apellido2, dni, telf) VALUES (null,'$nombre', '$apellido1', '$apellido2', '$dni', $telf);"); 
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE){
+            $this->db->trans_rollback();
+        } else {
+            $this->db->trans_commit();
+            $status = 1;
+        }
+        return $status;
+
+    }
 }
